deea81993f95db23b62c0a3d0f469c1fbb724d4d
Rory Hackney <72114389+roryhackney@users.noreply.github.com>
2023-12-14 16:32:10 -0800

    Add files via upload

---
+++ Profile.java
@@ -0,0 +1,157 @@
+
+
+import java.util.ArrayList;
+
+/**
+ * Profile class to hold user info and picture.
+ * @author Rory Hackney
+ * @author Jordan Fleming
+ * @version 1.0
+ * @since 12/10/2023
+ */
+public class Profile {
+    /**
+     * Status enum to mark online or offline
+     */
+    public enum Status {ONLINE, OFFLINE}
+
+    /**
+     * Static int for number of profiles created so far to use for id.
+     */
+    private static int numProfilesConstructed = 0;
+    /**
+     * Holder variable for the id number of the profile.
+     */
+    private int id; //use to uniquely identify profiles so accounts with the same name can be created?
+    /**
+     * Name of the person who owns the profile.
+     */
+    private String name;
+    /**
+     * Picture to add to profile.
+     */
+    private String picture;
+    /**
+     * The current status of the user/profile.
+     */
+    private Status status;
+    /**
+     * ArrayList to hold all friends.
+     */
+    private ArrayList<Profile> friendProfiles;
+
+    /**
+     * Full constructor for Profile class.
+     * @param name      String name to attach to profile
+     * @param pic       String url of pic to add to profile.
+     */
+    public Profile(String name, String pic) {
+        numProfilesConstructed++;
+        id = numProfilesConstructed;
+        this.name = name;
+        this.picture = pic;
+        this.status = Status.ONLINE;
+        this.friendProfiles = new ArrayList<Profile>();
+    }
+
+    /**
+     * Constructor for Profile class.
+     * @param name      String name to attach to profile.
+     */
+    public Profile(String name) {
+        numProfilesConstructed++;
+        id = numProfilesConstructed;
+        this.name = name;
+        this.picture = "";
+        this.status = Status.ONLINE;
+        this.friendProfiles = new ArrayList<Profile>();
+    }
+
+    /**
+     * Sets the name variable of the profile to a given name
+     * @param name      String name to use to update profile.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns the name associated with the profile.
+     * @return      String name currently attached to the profile.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the status of the profile to online or offline
+     * @param status    The desired STATUS for the profile.
+     */
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    /**
+     * Returns the current status of the profile.
+     * @return      Enum Status of the profile.
+     */
+    public Profile.Status getStatus() {
+        return status;
+    }
+
+    /**
+     * Adds a friend to the profiles list of friends.
+     * @param friend      Profile of the friend to be added
+     * @return            the Profile of the friend added.
+     */
+    public Profile addFriend(Profile friend) {
+        friendProfiles.add(friend);
+        return friend;
+    }
+
+    /**
+     * Returns a list of all the friends added to this profile.
+     * @return      A string list of all the friends using their toString method
+     */
+    public String printFriends() {
+        StringBuilder friendString = new StringBuilder();
+        friendString.append("Friends List: \n");
+        for (Profile friend : friendProfiles) {
+            friendString.append(friend.toString()).append("\n");
+        }
+        return friendString.toString();
+    }
+
+    /**
+     * Returns the picture url
+     * @return      String associated with the picture of the profile
+     */
+    public String getPicture() {
+        return picture;
+    }
+
+    /**
+     * Returns the id associated with the profile.
+     * @return      int ID of the profile
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Returns a string with all the relevant profile data.
+     * @return      String representation of the profile.
+     */
+    public String toString() {
+        return "User ID: " + id + ", Name: " + name + ", Picture: " + picture + ", Status: " + status;
+    }
+
+    /**
+     * Returns a copy of the list of friends for this profile
+     * @return a copy of the list of friends for this profile
+     */
+    public ArrayList<Profile> getFriends() {
+        //copy so can't alter list of friends for this profile
+        return new ArrayList<Profile>(friendProfiles);
+    }
+}
---
+++ ProfileManager.java
@@ -0,0 +1,105 @@
+import ADTPackage.QueueInterface;
+import GraphPackage.UndirectedGraph;
+import java.util.Iterator;
+import ADTPackage.UnsortedLinkedDictionary;
+/**
+ * ProfileManager class to manage Profiles for social network
+ * @author Rory Hackney
+ */
+public class ProfileManager {
+    /** UndirectedGraph to hold Profile objects*/
+    private UndirectedGraph<Profile> profileGraph;
+    /** Dictionary that holds all profiles searchable by ID */
+    private UnsortedLinkedDictionary<Integer, Profile> allProfiles;
+
+    /**
+     * Sole Constructor for ProfileManager class.
+     */
+    public ProfileManager() {
+        profileGraph = new UndirectedGraph<>();
+        allProfiles = new UnsortedLinkedDictionary<>();
+    }
+
+    /**
+     * Adds a new Profile to the graph/list of users
+     * @param profile the Profile to be added, will not be added if null
+     */
+    public void addProfile(Profile profile) {
+        if (profile != null) {
+            profileGraph.addVertex(profile);
+            allProfiles.add(profile.getId(), profile);
+        }
+    }
+
+    /**
+     * Returns a String of all the Profiles in the graph/list.
+     * @return a String representation of all the Profiles
+     */
+    public String displayProfiles() {
+        StringBuilder profilesString = new StringBuilder();
+        profilesString.append("Current users: ").append(profileGraph.getNumberOfVertices()).append("\n");
+        Iterator<Profile> iter = allProfiles.getValueIterator();
+        while (iter.hasNext()) {
+            profilesString.append(iter.next()).append("\n");
+        }
+        return profilesString.toString();
+    }
+
+    /**
+     * Gives a string of all profiles and all their connected friends.
+     * @return              String representation of all profiles with their associated edges.
+     */
+    public String displayProfilesAndFriends() {
+        StringBuilder profilesString = new StringBuilder();
+        profilesString.append("All profiles: \n");
+        Iterator<Profile> iter = allProfiles.getValueIterator();
+        while (iter.hasNext()) {
+            Profile currentProfile = iter.next();
+            profilesString.append(currentProfile).append("\n");
+            profilesString.append("Friends of ").append(currentProfile.getName()).append(":\n");
+            profilesString.append(currentProfile.printFriends());
+        }
+        return  profilesString.toString();
+    }
+
+    /**
+     * Gives a string of all profiles and all their connected friends using a Breadth First Traversal
+     * @param profile       Profile to begin traversal from
+     * @return              String representation of all profiles with their associated edges.
+     */
+    public String displayProfilesBreadthFirst(Profile profile) {
+        StringBuilder profilesString = new StringBuilder();
+        profilesString.append("All profiles: \n");
+        QueueInterface<Profile> breadthFirst = profileGraph.getBreadthFirstTraversal(profile);
+        return profilesString.toString();
+    }
+
+    /**
+     * Removes a profile from the graph
+     * @param profile       Profile to be removed.
+     */
+    public void removeProfile(Profile profile) {
+//        profileGraph.remove(profile);
+    }
+
+    /**
+     * Returns the profile with the given id, or null if user doesn't exist
+     * @param id the id of the profile, should be a number
+     * @return the profile with the given id, or null if profile doesn't exist
+     */
+    public Profile getUser(int id) {
+        return allProfiles.getValue(id);
+    }
+
+    public boolean addFriendship(Profile friend1, Profile friend2) {
+        boolean result = false;
+        if (friend1 != null && friend2 != null) {
+            result = profileGraph.addEdge(friend1, friend2);
+            if (result) {
+                friend1.addFriend(friend2);
+                friend2.addFriend(friend1);
+            }
+        }
+        return result;
+    }
+}
